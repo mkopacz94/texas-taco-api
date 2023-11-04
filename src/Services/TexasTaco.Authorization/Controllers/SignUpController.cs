5bbdf71f2fd6fb87e635a7f1b7fc8e2a35cf573c
m.kopacz94 <m.kopacz94@gmail.com>
2023-11-04 16:03:23 +0100

    Authorization service created.

---
+++ src/Services/TexasTaco.Authorization/Controllers/SignUpController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TexasTaco.Authorization.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    internal class SignUpController : ControllerBase
+    {
+    }
+}
