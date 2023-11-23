2f88035b4a915e548d724efbeeb8ae7f34bf5332
m.kopacz94 <m.kopacz94@gmail.com>
2023-11-23 18:09:58 +0100

    Created initial Authentication service.

--- src/Authentication/TexasTaco.Authentication.Api/Program.cs
+++ src/Authentication/TexasTaco.Authentication.Api/Program.cs
@@ -1,6 +1,11 @@
+using TexasTaco.Authentication.Core;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services
+    .AddTexasTacoAuthentication(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
