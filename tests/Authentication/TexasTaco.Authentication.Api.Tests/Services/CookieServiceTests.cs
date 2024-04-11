8661d7e1be9371c5ee7076abacef318382272a43
m.kopacz94 <m.kopacz94@gmail.com>
2024-04-11 19:32:49 +0200

    CookieService unit tests created.

--- tests/Authentication/TexasTaco.Authentication.Api.Tests/Services/CookieServiceTests.cs
+++ tests/Authentication/TexasTaco.Authentication.Api.Tests/Services/CookieServiceTests.cs
@@ -1,12 +1,97 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using NSubstitute;
+using TexasTaco.Authentication.Api.Services;
 
 namespace TexasTaco.Authentication.Api.Tests.Services
 {
     public class CookieServiceTests
     {
+        private readonly IHttpContextAccessor _httpContextAccessorMock
+            = Substitute.For<IHttpContextAccessor>();
+
+        [Fact]
+        public void GetCookie_Should_ReturnNull_IfCookieNotExist()
+        {
+            //Arrange
+            var httpContext = new DefaultHttpContext();
+
+            _httpContextAccessorMock.HttpContext
+                .Returns(httpContext);
+
+            var cookieService = new CookieService(_httpContextAccessorMock);
+
+            //Act
+            string? cookieValue = cookieService.GetCookie("cookie");
+
+            //Assert
+            cookieValue
+                .Should()
+                .BeNull();
+        }
+
+        [Fact]
+        public void GetCookie_Should_ReturnCookieValue_IfCookieExists()
+        {
+            //Arrange
+            string cookieName = "cookie";
+            string expectedCookieValue = "cookieValue";
+
+            var cookieCollectionMock = new FakeCookieCollection
+            {
+                { cookieName, expectedCookieValue }
+            };
+
+            var httpContext = new DefaultHttpContext();
+            httpContext.Request.Cookies = cookieCollectionMock;
+
+            _httpContextAccessorMock.HttpContext
+                .Returns(httpContext);
+
+            var cookieService = new CookieService(_httpContextAccessorMock);
+
+            //Act
+            string? cookieValue = cookieService.GetCookie(cookieName);
+
+            //Assert
+            cookieValue
+                .Should()
+                .Be(expectedCookieValue);
+        }
+
+        [Fact]
+        public void SetCookie_Should_SetCookieWithOptionsInHttpContextResponse()
+        {
+            //Arrange
+            string cookieName = "cookie";
+            string cookieValue = "cookieValue";
+
+            var httpContext = new DefaultHttpContext();
+
+            _httpContextAccessorMock.HttpContext
+                .Returns(httpContext);
+
+            var cookieService = new CookieService(_httpContextAccessorMock);
+
+            //Act
+            var cookieOptions = new CookieOptions
+            {
+                Domain = "test",
+                Expires = new DateTime(2024, 4, 11)
+            };
+
+            cookieService.SetCookie(cookieName, cookieValue, cookieOptions);
+
+            //Assert
+            httpContext.Response.Headers.SetCookie[0]
+                .Should()
+                .Be("cookie=cookieValue; expires=Wed, 10 Apr 2024 22:00:00 GMT; domain=test; path=/");
+        }
+
+        internal class FakeCookieCollection : Dictionary<string, string>, IRequestCookieCollection
+        {
+            ICollection<string> IRequestCookieCollection.Keys =>
+                ((Dictionary<string, string>)this).Keys;
+        }
     }
 }
