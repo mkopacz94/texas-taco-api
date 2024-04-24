2ee6a073bc899dc39f180d89380ba2d4077b622e
m.kopacz94 <m.kopacz94@gmail.com>
2024-04-24 20:17:42 +0200

    CookieClaimsService tests created.

--- tests/Authentication/TexasTaco.Authentication.Api.Tests/Services/CookieClaimsServiceTests.cs
+++ tests/Authentication/TexasTaco.Authentication.Api.Tests/Services/CookieClaimsServiceTests.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Http;
 using NSubstitute;
+using System.Security.Claims;
 using TexasTaco.Authentication.Api.Services;
 using TexasTaco.Authentication.Core.Entities;
 using TexasTaco.Shared.Authentication;
@@ -10,29 +12,41 @@ namespace TexasTaco.Authentication.Api.Tests.Services
 {
     public class CookieClaimsServiceTests
     {
+        private readonly HttpContext _httpContextMock
+            = Substitute.For<HttpContext>();
+
         private readonly IHttpContextAccessor _httpContextAccessorMock
             = Substitute.For<IHttpContextAccessor>();
 
-        [Fact]
-        public async Task SetAccountClaims_Should_SetEmailValueAsEmailClaim()
+        private readonly IAuthenticationService _authenticationServiceMock
+            = Substitute.For<IAuthenticationService>();
+
+        public CookieClaimsServiceTests()
         {
-            //Arrange
-            var httpContextMock = Substitute.For<HttpContext>();
-
             _httpContextAccessorMock.HttpContext
-                .Returns(httpContextMock);
+                .Returns(_httpContextMock);
 
-            var authSerivceMock = Substitute.For<IAuthenticationService>();
-            authSerivceMock
+            _authenticationServiceMock
                 .SignInAsync(default!, default, default!, default)
                 .ReturnsForAnyArgs(Task.CompletedTask);
 
             var serviceProvider = Substitute.For<IServiceProvider>();
             serviceProvider.GetService(typeof(IAuthenticationService))
-                .Returns(authSerivceMock);
+                .Returns(_authenticationServiceMock);
 
-            httpContextMock.RequestServices
+            _httpContextMock.RequestServices
                 .Returns(serviceProvider);
+        }
+
+        [Fact]
+        public async Task SetAccountClaims_Should_SignInWithAllRequiredClaimsTypes()
+        {
+            //Arrange
+            List<string> requiredClaimsTypes = [
+                ClaimTypes.Email,
+                TexasTacoClaimNames.AccountId,
+                ClaimTypes.Role
+            ];
 
             var claimsService = new CookieClaimsService(_httpContextAccessorMock);
 
@@ -46,10 +60,119 @@ namespace TexasTaco.Authentication.Api.Tests.Services
             await claimsService.SetAccountClaims(account);
 
             //Assert
-            await httpContextMock
-                .ReceivedWithAnyArgs(1)
+            await _authenticationServiceMock
+                .Received(1)
                 .SignInAsync(
-                    default, default!, default);
+                    _httpContextMock,
+                    CookieAuthenticationDefaults.AuthenticationScheme,
+                    Arg.Is<ClaimsPrincipal>(p => p.Claims
+                        .All(c => requiredClaimsTypes.Contains(c.Type ))),
+                    Arg.Any<AuthenticationProperties>());
+        }
+
+        [Fact]
+        public async Task SetAccountClaims_Should_SignInWithEmailValueAsEmailClaim()
+        {
+            //Arrange
+            string email = "test@domain.com";
+
+            var claimsService = new CookieClaimsService(_httpContextAccessorMock);
+
+            var account = new Account(
+                new EmailAddress(email),
+                Role.Customer,
+                [],
+                []);
+
+            //Act
+            await claimsService.SetAccountClaims(account);
+
+            //Assert
+            await _authenticationServiceMock
+                .Received(1)
+                .SignInAsync(
+                    _httpContextMock, 
+                    CookieAuthenticationDefaults.AuthenticationScheme, 
+                    Arg.Is<ClaimsPrincipal>(p => p.Claims
+                        .Any(c => c.Type == ClaimTypes.Email && c.Value == email)), 
+                    Arg.Any<AuthenticationProperties>());
+        }
+
+        [Fact]
+        public async Task SetAccountClaims_Should_SignInWithAccountIdValueAsAccountIdClaim()
+        {
+            //Arrange
+            var claimsService = new CookieClaimsService(_httpContextAccessorMock);
+
+            var account = new Account(
+                new EmailAddress("test@domain.com"),
+                Role.Customer,
+                [],
+                []);
+
+            //Act
+            await claimsService.SetAccountClaims(account);
+
+            //Assert
+            await _authenticationServiceMock
+                .Received(1)
+                .SignInAsync(
+                    _httpContextMock,
+                    CookieAuthenticationDefaults.AuthenticationScheme,
+                    Arg.Is<ClaimsPrincipal>(p => p.Claims
+                        .Any(c => c.Type == TexasTacoClaimNames.AccountId && c.Value == account.Id.Value.ToString())),
+                    Arg.Any<AuthenticationProperties>());
+        }
+
+        [Fact]
+        public async Task SetAccountClaims_Should_SignInWithRoleStringAsRoleClaim()
+        {
+            //Arrange
+            var claimsService = new CookieClaimsService(_httpContextAccessorMock);
+
+            var account = new Account(
+                new EmailAddress("test@domain.com"),
+                Role.Customer,
+                [],
+                []);
+
+            //Act
+            await claimsService.SetAccountClaims(account);
+
+            //Assert
+            await _authenticationServiceMock
+                .Received(1)
+                .SignInAsync(
+                    _httpContextMock,
+                    CookieAuthenticationDefaults.AuthenticationScheme,
+                    Arg.Is<ClaimsPrincipal>(p => p.Claims
+                        .Any(c => c.Type == ClaimTypes.Role && c.Value == account.Role.ToString())),
+                    Arg.Any<AuthenticationProperties>());
+        }
+
+        [Fact]
+        public async Task SetAccountClaims_Should_SignInWithIsPersistentAuthenticationProperties()
+        {
+            //Arrange
+            var claimsService = new CookieClaimsService(_httpContextAccessorMock);
+
+            var account = new Account(
+                new EmailAddress("test@domain.com"),
+                Role.Customer,
+                [],
+                []);
+
+            //Act
+            await claimsService.SetAccountClaims(account);
+
+            //Assert
+            await _authenticationServiceMock
+                .Received(1)
+                .SignInAsync(
+                    _httpContextMock,
+                    CookieAuthenticationDefaults.AuthenticationScheme,
+                    Arg.Any<ClaimsPrincipal>(),
+                    Arg.Is<AuthenticationProperties>(p => p.IsPersistent));
         }
     }
 }
