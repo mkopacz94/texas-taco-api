87b224093736346fae91b7978234a55b57bd628f
m.kopacz94 <m.kopacz94@gmail.com>
2024-11-07 17:09:05 +0100

    Add OrdersDbContext.

---
+++ src/Orders/TexasTaco.Orders.Infrastructure/Data/EF/OrdersDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TexasTaco.Orders.Domain.Basket;
+
+namespace TexasTaco.Orders.Infrastructure.Data.EF
+{
+    internal class OrdersDbContextt(DbContextOptions<OrdersDbContextt> options)
+        : DbContext(options)
+    {
+        public DbSet<BasketItem> BasketItems { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+        }
+    }
+}
