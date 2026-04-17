847f5ddd186e688a82f2bad25724dada10606622
m.kopacz94 <m.kopacz94@gmail.com>
2026-04-17 18:08:27 +0200

    Add verification token url.

--- src/Authentication/TexasTaco.Authentication.Core/Services/Verification/EmailVerificationService.cs
+++ src/Authentication/TexasTaco.Authentication.Core/Services/Verification/EmailVerificationService.cs
@@ -17,23 +17,28 @@ namespace TexasTaco.Authentication.Core.Services.Verification
 
             var emailNotification = new EmailNotification(
                 "Texas Taco account verification",
-                CreateVerificationEmailBody(),
+                CreateVerificationEmailBody(verificationToken),
                 new EmailAddress("texastaco.notifications@gmail.com"),
                 account.Email);
 
             await _emailNotificationsRepository.AddAsync(emailNotification);
         }
 
-        private static string CreateVerificationEmailBody()
+        private static string CreateVerificationEmailBody(VerificationToken token)
         {
-            return @"
+            return $@"
                 <div style='font-family: 'Helvetica';'>
                     <p style='font-size: 1.5em;'>Texas Taco</p>
                     <p>Hi,</p>
                     <p>Thank you for signing up for <span style='font-weight: bold;'>Texas Taco</span>. To activate your account, please verify your email address.</p>
-                    <a href='https://www.google.com/'><button style='padding: 0.8em 1.5em; border-radius: 50px; border: 0px; color: white; cursor: pointer; background: #ffa436;'>Verify Now</button></a>
+                    <a href='{BuildVerificationUrl(token)}'><button style='padding: 0.8em 1.5em; border-radius: 50px; border: 0px; color: white; cursor: pointer; background: #ffa436;'>Verify Now</button></a>
                     <p style='font-size: 0.8em; margin-top: 3em;'>Ignore this email if you received it without signing up.</p>
                 </div>";
+        }
+
+        private static string BuildVerificationUrl(VerificationToken token)
+        {
+            return $"http://localhost:5173/verify?token={token.Token}";
         }
     }
 }
